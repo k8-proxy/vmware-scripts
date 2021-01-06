c18c2e7b2e39461384c904fcba745ed5ee7c1d89
Jakub Krzywda <jakub@elastisys.com>
2021-01-06 12:56:54 +0100

    Fix network

--- scripts/network/network.sh
+++ scripts/network/network.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Network setup
 DEBIAN_FRONTEND=noninteractive
-sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
-update-grub
 rm -f /etc/netplan/*.yml /etc/netplan/*.yaml
 cat > /etc/netplan/network.yaml <<EOF
 network:
   version: 2
   ethernets:
-    eth0:
-      dhcp4: true
+    ens33:
+      dhcp4: false
+      addresses:
+      - 51.89.210.150/28 # Replace with IP Address / Netmask bits (CIDR format), ex: 192.168.1.2/24
+      gateway4: 51.89.210.158 # Replace with Gateway
+      nameservers:
+        addresses:
+        - 8.8.4.4 # Replace with DNS resolver
 EOF
-
+netplan apply
