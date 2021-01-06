01e7b86f3af2412d61be2dd611acfa31e8cef101
Faisal Adnan <68223517+faisaladnanpeltops@users.noreply.github.com>
2021-01-06 19:55:34 +0100

    fixed install problems

--- scripts/agent/fluentbit.sh
+++ scripts/agent/fluentbit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Agent install
-apt-get update
 wget -qO - https://packages.fluentbit.io/fluentbit.key | sudo apt-key add -
-echo "deb https://packages.fluentbit.io/ubuntu/focal focal main" >>  /etc/apt/sources.list
-apt-get install -y td-agent-bit
-service td-agent-bit start
+sudo sh -c 'echo "deb https://packages.fluentbit.io/ubuntu/focal focal main" >>  /etc/apt/sources.list'
+sudo apt-get update
+sudo apt-get install -y td-agent-bit
+sudo service td-agent-bit start
