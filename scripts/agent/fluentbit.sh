d87a675e061e947d4c6fe88a61e91a75921f064a
Faisal Adnan <68223517+faisaladnanpeltops@users.noreply.github.com>
2021-01-13 13:17:20 +0100

    improve dockerization

--- scripts/agent/fluentbit.sh
+++ scripts/agent/fluentbit.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Agent install
+set -euxc
 wget -qO - https://packages.fluentbit.io/fluentbit.key | sudo apt-key add -
-sudo sh -c 'echo "deb https://packages.fluentbit.io/ubuntu/focal focal main" >>  /etc/apt/sources.list'
+echo 'deb https://packages.fluentbit.io/ubuntu/focal focal main' | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y td-agent-bit
 sudo service td-agent-bit start
+
+git clone https://github.com/k8-proxy/k8-vmware.git
+cd k8-vmware
+sudo apt-get install -y python3 python3-pip
+pip3 install -r requirements.txt
+pip3 install -e . 
