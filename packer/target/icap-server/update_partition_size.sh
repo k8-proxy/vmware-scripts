8d76ecf8cde55b949f845cdcf0203172f37b3776
Pranay <pranaysahith7@gmail.com>
2021-02-10 12:38:18 +0000

    get partition name at runtime

--- packer/target/icap-server/update_partition_size.sh
+++ packer/target/icap-server/update_partition_size.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # increase partition size to maximum disk size
+partition_name=$(df -h | grep -e /$ | cut -d" " -f1)
+disk_name=${partition_name:0: -1}
+partition_number=${partition_name: -1}
+growpart $disk_name $partition_number
+resize2fs $partition_name
+
 sudo tee -a /etc/init.d/update_partition <<EOF
 #!/bin/bash
 
@@ -13,8 +19,8 @@ sudo tee -a /etc/init.d/update_partition <<EOF
 # Short-Description:    updates partition 
 # Description:          size to maximum disk size
 ### END INIT INFO
-growpart /dev/sda 1
-resize2fs /dev/sda1
+growpart $disk_name $partition_number
+resize2fs $partition_name
 EOF
 sudo chmod +x /etc/init.d/update_partition
 sudo update-rc.d update_partition defaults
