8463809237cf8597a8895a4eead7e6ea5c6a9345
Pranay <pranaysahith7@gmail.com>
2021-02-15 03:40:20 +0000

    use systemd for process

--- packer/target/filedrop/flush_ip.sh
+++ packer/target/filedrop/flush_ip.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-sudo iptables --flush
-sudo iptables -tnat --flush
-
-sudo tee -a /etc/init.d/flush_iptables <<EOF
+cat > /home/centos/flush_iptables.sh <<EOF
 #!/bin/bash
-### BEGIN INIT INFO
-# Provides:             flush_iptables
-# Required-Start:       $local_fs $remote_fs $network $syslog $named
-# Required-Stop:        $local_fs $remote_fs $network $syslog $named
-# Default-Start:        2 3 4 5
-# Default-Stop:         
-# Short-Description:    flush iptables
-# Description:          flush iptables
-### END INIT INFO
 sudo iptables --flush
 sudo iptables -tnat --flush
 EOF
-sudo chmod +x /etc/init.d/flush_iptables
-sudo update-rc.d flush_iptables defaults
+chmod +x /home/centos/flush_iptables.sh
+/home/centos/flush_iptables.sh
+
+sudo tee -a /etc/systemd/system/flush_iptables.service <<EOF
+ [Unit]
+Description=Flush iptables
+After=network.target
+
+[Service]
+Type=simple
+User=centos
+ExecStart=/home/centos/flush_iptables.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable flush_iptables.service
