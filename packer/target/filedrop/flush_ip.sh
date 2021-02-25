1431e9741517c8f8c5a24e9edf5ca961dc81e265
nirvishek <nirvishek@smalldaytech.com>
2021-02-25 14:35:47 +0530

    Coredns and nginx pod fail fix - filedrop centos7

--- packer/target/filedrop/flush_ip.sh
+++ packer/target/filedrop/flush_ip.sh
@@ -4,6 +4,14 @@ cat > /home/centos/flush_iptables.sh <<EOF
 #!/bin/bash
 sudo iptables --flush
 sudo iptables -tnat --flush
+
+kubectl scale --replicas=0 deployment.apps/k8-rebuild-sow-rest
+kubectl scale --replicas=0 ddeployment.apps/sow-rest-api
+kubectl scale --replicas=0 deployment.apps/sow-rest-ui
+
+kubectl scale --replicas=1 deployment.apps/k8-rebuild-sow-rest
+kubectl scale --replicas=1 ddeployment.apps/sow-rest-api
+kubectl scale --replicas=1 deployment.apps/sow-rest-ui
 EOF
 chmod +x /home/centos/flush_iptables.sh
 /home/centos/flush_iptables.sh
