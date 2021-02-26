11443b66de39e206d3234b20e1842df858d7a178
Pranay <pranaysahith7@gmail.com>
2021-02-26 19:14:57 +0000

    fix installation of yq and k3s network issue

--- packer/target/icap-server/flush_ip.sh
+++ packer/target/icap-server/flush_ip.sh
@@ -4,9 +4,10 @@ cat > /home/centos/flush_iptables.sh <<EOF
 #!/bin/bash
 sudo iptables --flush
 sudo iptables -tnat --flush
+sudo systemctl restart k3s
 EOF
 chmod +x /home/centos/flush_iptables.sh
-/home/centos/flush_iptables.sh
+# /home/centos/flush_iptables.sh
 
 sudo tee -a /etc/systemd/system/flush_iptables.service <<EOF
  [Unit]
@@ -20,7 +21,7 @@ ExecStart=/home/centos/flush_iptables.sh
 RemainAfterExit=yes
 
 [Install]
-WantedBy=multi-user.target
+WantedBy=k3s.service
 EOF
 sudo systemctl daemon-reload
 sudo systemctl enable flush_iptables.service
