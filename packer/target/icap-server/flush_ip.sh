0afa45f46eefe5f029af42276c365621cfb2f1b3
Nader Ali <58347752+naderaly@users.noreply.github.com>
2021-02-28 17:51:11 +0200

    Update flush_ip.sh

--- packer/target/icap-server/flush_ip.sh
+++ packer/target/icap-server/flush_ip.sh
@@ -4,23 +4,22 @@ cat > /home/centos/flush_iptables.sh <<EOF
 #!/bin/bash
 sudo iptables --flush
 sudo iptables -tnat --flush
+sudo systemctl restart k3s
 EOF
 chmod +x /home/centos/flush_iptables.sh
-/home/centos/flush_iptables.sh
+# /home/centos/flush_iptables.sh
 
 sudo tee -a /etc/systemd/system/flush_iptables.service <<EOF
  [Unit]
 Description=Flush iptables
 After=network.target
-
 [Service]
 Type=simple
 User=centos
 ExecStart=/home/centos/flush_iptables.sh
 RemainAfterExit=yes
-
 [Install]
-WantedBy=multi-user.target
+WantedBy=k3s.service
 EOF
 sudo systemctl daemon-reload
 sudo systemctl enable flush_iptables.service
