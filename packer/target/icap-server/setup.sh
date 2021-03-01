fe56d38beb80aef1b8c0b158e6646dd18a8ea656
Pranay <pranaysahith7@gmail.com>
2021-03-01 15:57:12 +0000

    remove monitoring

--- packer/target/icap-server/setup.sh
+++ packer/target/icap-server/setup.sh
@@ -142,8 +142,8 @@ kubectl create -n management-ui secret generic smtpsecret \
 cd ~
 
 # deploy monitoring solution
-git clone https://github.com/k8-proxy/k8-rebuild.git && cd k8-rebuild
-helm install sow-monitoring monitoring --set monitoring.elasticsearch.host=$MONITORING_IP --set monitoring.elasticsearch.username=$MONITORING_USER --set monitoring.elasticsearch.password=$MONITORING_PASSWORD
+# git clone https://github.com/k8-proxy/k8-rebuild.git && cd k8-rebuild
+# helm install sow-monitoring monitoring --set monitoring.elasticsearch.host=$MONITORING_IP --set monitoring.elasticsearch.username=$MONITORING_USER --set monitoring.elasticsearch.password=$MONITORING_PASSWORD
 
 # wait until the pods are up
 sleep 300s
