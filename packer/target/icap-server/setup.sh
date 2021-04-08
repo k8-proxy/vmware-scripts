221db96d5e5137359801a64f7fd1dcf66f06a96c
Gurunath <guru9794@gmail.com>
2021-04-08 16:36:27 +0530

    Updated keyring

--- packer/target/icap-server/setup.sh
+++ packer/target/icap-server/setup.sh
@@ -135,7 +135,8 @@ helm install sow-monitoring monitoring --set monitoring.elasticsearch.host=$MONI
 # wait until the pods are up
 # sleep 120s
 #Install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io
