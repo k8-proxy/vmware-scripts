e8062ced2937b6a07497eed9e74f167592d49b15
Gurunath <guru9794@gmail.com>
2021-04-08 16:42:34 +0530

    Fixed apt install

--- packer/target/icap-server/setup.sh
+++ packer/target/icap-server/setup.sh
@@ -139,7 +139,7 @@ sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
