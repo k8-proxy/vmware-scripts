b2dc798e9f189387325b61ef7f4dade694830d7e
Gurunath <guru9794@gmail.com>
2021-04-15 12:16:13 +0530

    Fixed OVA password

--- packer/setSshPasswd.sh
+++ packer/setSshPasswd.sh
@@ -3,9 +3,9 @@ source /home/ubuntu/.env
 SSH_PASSWORD=${SSH_PASSWORD:-glasswall}
 printf "${SSH_PASSWORD}\n${SSH_PASSWORD}" | passwd ubuntu
 usermod -U ubuntu
-useradd -m -s /bin/bash glasswall
-usermod -aG sudo glasswall
-printf "${SSH_PASSWORD}\n${SSH_PASSWORD}" | passwd glasswall
+# useradd -m -s /bin/bash glasswall
+# usermod -aG sudo glasswall
+# printf "${SSH_PASSWORD}\n${SSH_PASSWORD}" | passwd glasswall
 sed -i "s/.*PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sed -i "s/lock_passwd.*/lock_passwd: False/g" /etc/cloud/cloud.cfg
 service ssh restart
