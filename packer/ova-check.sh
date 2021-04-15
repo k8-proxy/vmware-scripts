3e690c0732f09cb2696c2e1214d4ab99812136e5
Gurunath <guru9794@gmail.com>
2021-04-15 11:36:24 +0530

    Fixed OVA issues

--- packer/ova-check.sh
+++ packer/ova-check.sh
@@ -21,4 +21,3 @@ echo "Upload Completed !!!"
 BUCKET=$(cat ova-export.json | jq -r .S3Bucket)
 OBJECT="$(aws s3 ls $BUCKET/vms/$VM_NAME/ | sort | tail -n 1 | awk '{print $4}')"
 aws s3 mv s3://${BUCKET}/vms/$VM_NAME/${OBJECT} s3://${BUCKET}/vms/$VM_NAME/${OVA_NAME}
-rm out.json
