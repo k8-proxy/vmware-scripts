705e3f01abc1e125db253be9157c9b7c053bbbff
Gurunath <guru9794@gmail.com>
2021-04-19 23:31:24 +0530

    Wizard installation

--- packer/packer-icapshell.sh
+++ packer/packer-icapshell.sh
@@ -69,3 +69,48 @@ echo $CS_API_IMAGE
 sed -i 's|glasswallsolutions/cs-k8s-api:latest|'$CS_API_IMAGE'|' deployment.yaml
 kubectl  apply -f deployment.yaml -n icap-adaptation
 kubectl patch svc proxy-rest-api -n icap-adaptation --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":8080}]'
+
+# defining vars
+DEBIAN_FRONTEND=noninteractive
+KERNEL_BOOT_LINE='net.ifnames=0 biosdevname=0'
+
+# cloning vmware scripts repo
+git clone --single-branch -b main https://github.com/k8-proxy/vmware-scripts.git ~/scripts
+
+# install needed packages
+apt install -y telnet tcpdump open-vm-tools net-tools dialog curl git sed grep fail2ban
+systemctl enable fail2ban.service
+tee -a /etc/fail2ban/jail.d/sshd.conf << EOF > /dev/null
+[sshd]
+enabled = true
+port = ssh
+action = iptables-multiport
+logpath = /var/log/auth.log
+bantime  = 10h
+findtime = 10m
+maxretry = 5
+EOF
+systemctl restart fail2ban
+
+# switching to predictable network interfaces naming
+grep "$KERNEL_BOOT_LINE" /etc/default/grub >/dev/null || sudo sed -Ei "s/GRUB_CMDLINE_LINUX=\"(.*)\"/GRUB_CMDLINE_LINUX=\"\1 $KERNEL_BOOT_LINE\"/g" /etc/default/grub
+
+# remove swap 
+swapoff -a && sudo rm -f /swap.img && sudo sed -i '/swap.img/d' /etc/fstab && echo Swap removed
+
+# update grub
+update-grub
+
+# installing the wizard
+install -T ~/scripts/scripts/wizard/wizard.sh /usr/local/bin/wizard -m 0755
+
+# installing initconfig ( for running wizard on reboot )
+cp -f ~/scripts/scripts/bootscript/initconfig.service /etc/systemd/system/initconfig.service
+install -T ~/scripts/scripts/bootscript/initconfig.sh /usr/local/bin/initconfig.sh -m 0755
+systemctl daemon-reload
+
+# enable initconfig for the next reboot
+systemctl enable initconfig
+
+# remove vmware scripts directory
+rm -rf ~/scripts/
